04634e2c091b5dbee7e22cf65686bf901c221e7e
Josh O <josuah9@hotmail.com>
2023-09-07 01:41:32 -0400

    Imported various assets, added logic for rotating and moving units, animations implemented for idle and moving. created action system script this will be used fo rmanagement
    Imported various assets, added logic for rotating and moving units, animations implemented for idle and moving. created action system script this will be used fo rmanagement

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     private void Update()
     {
+        
         float stoppingDistance = .1f;
         if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized; // only care about direction so we normalize
             float moveSpeed = 4;
             transform.position += moveDirection * moveSpeed * Time.deltaTime; // framerate independent movement
-
+                                                                              // using transform. forward/up/right * a vector allows for easy rotation
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward,moveDirection, rotateSpeed * Time.deltaTime); // we'll lerp rotation to make it smoother
+            unitAnimator.SetBool("isWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("isWalking", false);
         }
 
         if (Input.GetMouseButtonDown(0))
